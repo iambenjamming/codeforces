7ad8d3a195f80ad394cd497d26489ef38939001c
benjamin <chahuawang50@gmail.com>
2024-07-14 01:51:30 +0800

    yarr

---
+++ Codeforces/1100/interestingdrink.py
@@ -0,0 +1,16 @@
+import sys
+import bisect
+
+n = int(sys.stdin.readline())
+shops = list(map(int, sys.stdin.readline().split()))
+
+shops.sort()
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+
+    budget = int(sys.stdin.readline())
+    
+    index = bisect.bisect_right(shops, budget)
+    print(index)
