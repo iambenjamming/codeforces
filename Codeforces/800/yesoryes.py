8782484f33ff84a398b83b3f513d1ac05fb15b56
benjamin <chahuawang50@gmail.com>
2024-09-25 15:33:40 +0800

    quest

---
+++ Codeforces/800/yesoryes.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for _ in range(t):
+
+    s = input().upper()
+
+    if s == 'YES':
+        print(s)
+    else:
+        print('NO')
---
+++ Codeforces/900/gamewithsticks.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+
+e = min(m,n)
+
+if e&1:
+    print('Akshat')
+else:
+    print('Malvika')
